eca6de4bd91b5f8eda308a091bfbd2528c856df4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-27 17:38:29 +0530

    new json db to store settings

--- src/utils/database.py
+++ src/utils/database.py
@@ -5,3 +5,9 @@ class Embeds:
     with open("database/embeds.json", "r", encoding="utf-8") as _file:
         _data = json.load(_file)
         common = _data["common"]
+
+
+class Settings:
+    with open("database/settings.json", "r", encoding="utf-8") as _file:
+        _data = json.load(_file)
+        main = _data["main"]
