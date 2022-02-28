473e494b0e9a42d7fca1c938ebadb9ea3869127f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-28 21:52:57 +0530

    breed command is complete

--- src/cogs/TheCat.py
+++ src/cogs/TheCat.py
@@ -37,40 +37,6 @@ class TheCat(commands.Cog):
         os.system("rm -rf ./{delfname}".format(delfname=filename))
 
     @commands.command()
-    async def gif(self, ctx, *, text=None):
-        if text is None:
-            final_url = "https://cataas.com/cat/gif"
-        else:
-            final_url = f"https://cataas.com/cat/gif/says/{text}"
-
-        if self.saveGIF(url=final_url):
-            file = discord.File(f'temp.gif', filename="temp.gif")
-            embed = discord.Embed(title="a Cat",
-                                  color=0xcb42f5,
-                                  timestamp=datetime.utcnow())
-            embed.set_author(name=str(self.client.user.name),
-                             icon_url=str(self.client.user.avatar_url))
-            embed.set_image(url="attachment://temp.gif")
-            embed.set_footer(text=EmbedsDB.common["footer"].format(
-                author_name=ctx.author.name), icon_url=str(ctx.author.avatar_url))
-            await ctx.send(file=file, embed=embed)
-
-            self.removeImage(filename="temp.gif")
-
-        else:
-            embed = discord.Embed(title="An Error has Occured",
-                                  description="Unable to load the GIF from the API",
-                                  color=0xcb42f5,
-                                  timestamp=datetime.utcnow())
-            embed.set_author(name=str(self.client.user.name),
-                             icon_url=str(self.client.user.avatar_url))
-            embed.set_thumbnail(
-                url="https://cdn.discordapp.com/attachments/877796755234783273/879298565380386846/sign-red-error-icon-1.png")
-            embed.set_footer(text=EmbedsDB.common["footer"].format(
-                author_name=ctx.author.name), icon_url=str(ctx.author.avatar_url))
-            await ctx.send(embed=embed)
-
-    @commands.command()
     async def breed(self, ctx, *, text=None):
         url = "https://api.thecatapi.com/v1/breeds"
 
@@ -106,6 +72,121 @@ class TheCat(commands.Cog):
 
         data = result[random.randint(1, len(result) - 1)]
 
+        final_Links = ""
+        try:
+            final_Links += f"CFA: {data['cfa_url']}\n"
+        except:
+            pass
+        try:
+            final_Links += f"VetStreet: {data['vetstreet_url']}\n"
+        except:
+            pass
+        try:
+            final_Links += f"VCA Hospitals: {data['vcahospitals_url']}"
+        except:
+            pass
+
+        final_CountryOfOrigin = ""
+        try:
+            final_CountryOfOrigin += f"Origin: {data['origin']}\n"
+        except:
+            pass
+        try:
+            final_CountryOfOrigin += f"Country Code: {data['country_code']}"
+        except:
+            pass
+
+        final_Stats = ""
+        try:
+            final_Stats += f"Indoor: {data['indoor']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Lap: {data['lap']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Adaptability: {data['adaptability']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Affection Level: {data['affection_level']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Child Friendly: {data['child_friendly']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Dog Friendly: {data['dog_friendly']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Energy Level: {data['energy_level']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Grooming: {data['grooming']}"
+        except:
+            pass
+
+        try:
+            final_Stats += f"Health Issues: {data['health_issues']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Intelligence: {data['intelligence']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Shedding Level: {data['shedding_level']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Social Needs: {data['social_needs']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Stranger Friendly: {data['stranger_friendly']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Vocalisation: {data['vocalisation']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Experimental: {data['experimental']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Hairless: {data['hairless']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Natural: {data['natural']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Rare: {data['rare']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Rex: {data['rex']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Suppressed Tail: {data['suppressed_tail']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Short Legs: {data['short_legs']}\n"
+        except:
+            pass
+        try:
+            final_Stats += f"Hyperallergenic: {data['hypoallergenic']}"
+        except:
+            pass
+
         embed = discord.Embed(title=f"{data['name']}",
                               description=f"{data['description']}",
                               color=0xcb42f5,
@@ -114,25 +195,22 @@ class TheCat(commands.Cog):
                          icon_url=str(self.client.user.avatar_url))
         embed.set_image(url=f"{data['image']['url']}")
         embed.add_field(name="Links",
-                        value=f"CFA: {data['cfa_url']}\nVetStreet: {data['vetstreet_url']}\nVCA Hospitals: {data['vcahospitals_url']}",
+                        value=final_Links,
+                        inline=False)
+        embed.add_field(name="Wikipedia",
+                        value=f"{data['wikipedia_url']}",
                         inline=False)
         embed.add_field(name="Temperament",
                         value=f"{data['temperament']}",
                         inline=False)
         embed.add_field(name="Country Of Origin",
-                        value=f"Origin: {data['origin']}\nCountry Code: {data['country_code']}",
+                        value=final_CountryOfOrigin,
                         inline=False)
         embed.add_field(name="Lifespan",
                         value=f"{data['life_span']}",
                         inline=False)
-        embed.add_field(name="Wikipedia",
-                        value=f"{data['wikipedia_url']}",
-                        inline=False)
-        embed.add_field(name="Stats 1",
-                        value=f"Indoor: {data['indoor']}\nLap: {data['lap']}\nAdaptability: {data['adaptability']}\nAffection Level: {data['affection_level']}\nChild Friendly: {data['child_friendly']}\nDog Friendly: {data['dog_friendly']}\nEnergy Level: {data['energy_level']}\nGrooming: {data['grooming']}",
-                        inline=False)
-        embed.add_field(name="Stats 2",
-                        value=f"Health Issues: {data['health_issues']}\nIntelligence: {data['intelligence']}\nShedding Level: {data['shedding_level']}\nSocial Needs: {data['social_needs']}\nStranger Friendly: {data['stranger_friendly']}\nVocalisation: {data['vocalisation']}\nExperimental: {data['experimental']}",
+        embed.add_field(name="Stats",
+                        value=final_Stats,
                         inline=False)
         embed.set_footer(text=EmbedsDB.common["footer"].format(
             author_name=ctx.author.name), icon_url=str(ctx.author.avatar_url))
