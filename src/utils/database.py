5249398935fcd9cbb0da503e8c754078c5be01af
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-03 08:58:01 -0500

    store max amount posisble

--- src/utils/database.py
+++ src/utils/database.py
@@ -7,6 +7,12 @@ class Embeds:
         common = _data["common"]
 
 
+class EmbedsCommands:
+    with open("database/embeds.json", "r", encoding="utf-8") as _file:
+        _data = json.load(_file)
+        common = _data["commands"]
+
+
 class Settings:
     with open("database/settings.json", "r", encoding="utf-8") as _file:
         _data = json.load(_file)
